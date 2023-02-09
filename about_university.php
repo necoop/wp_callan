dbd132a1242287cc87b5b1aeb67533fb2c4872b4
necoop <amok2005@gmail.com>
2023-02-09 19:05:19 +0700

    Релиз страницы с информацией об университете

--- about_university.php
+++ about_university.php
@@ -4,13 +4,16 @@ Template Name: Об университете
 */
 
 add_action('wp_enqueue_scripts', function () {
-    wp_enqueue_style('style-about-university', get_template_directory_uri() . '/assets/about_university/css/style.css');
     wp_enqueue_style('style-university', get_template_directory_uri() . '/assets/unis/css/style.css');
+    wp_enqueue_style('style-about-university', get_template_directory_uri() . '/assets/about_university/css/style.css');
     wp_enqueue_script('swiper-about-university', 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js', 'null', 'null', true);
-    wp_enqueue_script('slider-about-university', get_template_directory_uri() . '/assets/about_university/js/slider.js', array('swiper-bundle'), 'null', true);
+    wp_enqueue_script('slider-university-gallery', get_template_directory_uri() . '/assets/about_university/js/slider.js', array('swiper-about-university'), 'null', true);
 });
 
 get_header();
+
+// Подключение файла обработки массивов
+require(get_template_directory() . '/assets/unis/sort_arrays.php');
 
 if (isset($_GET['id'])) {
     $uni_id = $_GET['id'];
@@ -42,6 +45,8 @@ if ($myposts) {
                 'establish-year' => get_field('about_establish-year'),
                 'study-numbers' => get_field('about_study-numbers'),
                 'living-price' => get_field('about_living-price'),
+                'university-info' => get_field('university-info'),
+                'admission-documents' => get_field('admission-documents'),
                 'galerey' => get_post_gallery_images()
             ];
         }
@@ -59,18 +64,16 @@ wp_reset_postdata(); // Сбрасываем $post
         <div class="page-link col-12 tmp">
             <a href="<?php bloginfo('url'); ?>"><img src="<?php bloginfo('template_directory'); ?>/assets/img/ui/home.svg"><?php echo get_the_title(8); ?> &nbsp &nbsp ></a><a href="<?php the_permalink(249); ?>"><?php echo get_the_title(249); ?> &nbsp &nbsp ></a><?php echo $uni['name']; ?>
         </div>
-        <h2><?php echo $uni['name']; ?></h2>
+        <h2 class="unis__name"><?php echo $uni['name']; ?></h2>
         <div class="vector"></div>
         <div class="unis__info__outer">
             <div class="unis__info__top row">
                 <div class="col-lg-8 col-md-12">
                     <div class="unis__foto">
                         <div class="rating">
-                            <img src="./unis/img/star.svg">
-                            <img src="./unis/img/star.svg">
-                            <img src="./unis/img/star.svg">
-                            <img src="./unis/img/star.svg">
-                            <img src="./unis/img/star.svg">
+                            <? for ($i = 0; $i < $uni['popularity']; $i++) {
+                                echo ('<img src=' . get_bloginfo("template_directory") . '/assets/unis/img/star.svg>');
+                            } ?>
                         </div>
                         <img src="<? echo $uni['galerey'][0] ?>" alt="Фото университета">
                     </div>
@@ -81,7 +84,7 @@ wp_reset_postdata(); // Сбрасываем $post
                             <li class="unis__data__item" id="unis__data__item1">
                                 <div class="unis__data__inner">
                                     <div class="title">Расположение</div>
-                                    <div class="data"><? echo($uni['country'] . ', ' . $uni['address']); ?></div>
+                                    <div class="data"><? echo ($uni['country'] . ', ' . $uni['address']); ?></div>
                                 </div>
                             </li>
                             <li class="unis__data__item" id="unis__data__item2">
@@ -130,29 +133,7 @@ wp_reset_postdata(); // Сбрасываем $post
                     <div class="unis__description">
                         <h3>О ВУЗе</h3>
                         <div class="unis__text">
-                            <p>
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae voluptatibus
-                                tenetur totam adipisci eum neque vitae aliquid. Voluptatibus ipsam officiis sunt
-                                sint, eius quo quidem repellendus iure amet vero? Reprehenderit sunt obcaecati
-                                consequuntur molestiae error ad cum quas deleniti ipsam repellat voluptate natus
-                                labore consequatur, laboriosam, veniam voluptatibus vero aut!
-                            </p>
-                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio accusamus sed
-                                sequi soluta omnis vero ea accusantium corporis, quibusdam illum.</p>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta vitae sit quis aperiam
-                                eius placeat labore nam totam laboriosam? Autem quisquam animi, nisi deserunt nemo
-                                consectetur, illo fugit ullam consequatur quae laudantium. Facere commodi, possimus
-                                corporis ipsum enim debitis deserunt quidem exercitationem, asperiores aspernatur
-                                perferendis maxime! Eos rerum sint porro aliquid. Maiores sit dolores quia eum
-                                similique rem dolor deserunt eveniet facilis, veritatis doloribus ex provident in
-                                vitae vel aliquid sequi perferendis ipsum? Repudiandae praesentium incidunt magni et
-                                quidem, soluta alias? Inventore ducimus corporis quos, accusantium nisi magnam nulla
-                                unde est quas voluptatibus voluptas corrupti dolorem blanditiis, deserunt,
-                                voluptatum ab!</p>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, consequatur
-                                voluptates repellat nihil, repellendus quasi vel aliquam impedit doloremque
-                                architecto, earum est nesciunt suscipit eaque excepturi reprehenderit! Repellat,
-                                molestias in?</p>
+                            <? echo $uni['university-info']; ?>
                         </div>
                     </div>
                 </div>
@@ -160,11 +141,9 @@ wp_reset_postdata(); // Сбрасываем $post
                     <div class="document__box__inner">
                         <h3>Документы для поступления</h3>
                         <ul class="document__list">
-                            <li class="document__item">Название документа</li>
-                            <li class="document__item">Название документа</li>
-                            <li class="document__item">Название документа</li>
-                            <li class="document__item">Название документа</li>
-                            <li class="document__item">Название документа</li>
+                            <? foreach (removeSpacesAndConvertToArray($uni['admission-documents']) as $item) {
+                                echo ('<li class="document__item">' . $item . '</li>');
+                            } ?>
                         </ul>
                     </div>
                 </div>
@@ -187,9 +166,9 @@ wp_reset_postdata(); // Сбрасываем $post
                             <div id="panel-question-1" class="accordion-collapse collapse question-collapse" aria-labelledby="panelsStayOpen-headingOne">
                                 <div class="accordion-body question-body">
                                     <ul>
-                                        <li>Бакалавриат</li>
-                                        <li>Аспирантура</li>
-                                        <li>Магистратура</li>
+                                        <? foreach (removeSpacesAndConvertToArray($uni['study_form']) as $item) {
+                                            echo ('<li>' . $item . '</li>');
+                                        } ?>
                                     </ul>
                                 </div>
                             </div>
@@ -213,22 +192,9 @@ wp_reset_postdata(); // Сбрасываем $post
                             <div id="panel-question-2" class="accordion-collapse collapse question-collapse" aria-labelledby="panelsStayOpen-headingOne">
                                 <div class="accordion-body question-body">
                                     <ul class="row speciality__row">
-                                        <li class="col-xl-3 col-md-6 col-sm-12">Архитектура</li>
-                                        <li class="col-xl-3 col-md-6 col-sm-12">Электроника</li>
-                                        <li class="col-xl-3 col-md-6 col-sm-12">Теплотехника</li>
-                                        <li class="col-xl-3 col-md-6 col-sm-12">Архитектура</li>
-                                        <li class="col-xl-3 col-md-6 col-sm-12">Электроника</li>
-                                        <li class="col-xl-3 col-md-6 col-sm-12">Теплотехника</li>
-                                        <li class="col-xl-3 col-md-6 col-sm-12">Архитектура</li>
-                                        <li class="col-xl-3 col-md-6 col-sm-12">Электроника и электротехника в
-                                            сельском хозяйстве</li>
-                                        <li class="col-xl-3 col-md-6 col-sm-12">Теплотехника</li>
-                                        <li class="col-xl-3 col-md-6 col-sm-12">Архитектура</li>
-                                        <li class="col-xl-3 col-md-6 col-sm-12">Электроника</li>
-                                        <li class="col-xl-3 col-md-6 col-sm-12">Теплотехника</li>
-                                        <li class="col-xl-3 col-md-6 col-sm-12">Архитектура</li>
-                                        <li class="col-xl-3 col-md-6 col-sm-12">Электроника</li>
-                                        <li class="col-xl-3 col-md-6 col-sm-12">Теплотехника</li>
+                                        <? foreach (removeSpacesAndConvertToArray($uni['speciality']) as $item) {
+                                            echo ('<li class="col-xl-3 col-md-6 col-sm-12">' . $item . '</li>');
+                                        } ?>
                                     </ul>
                                 </div>
                             </div>
@@ -247,19 +213,10 @@ wp_reset_postdata(); // Сбрасываем $post
                 <!-- Additional required wrapper -->
                 <div class="swiper-wrapper">
                     <!-- Slides -->
-                    <div class="swiper-slide">
-                        <img src="./uni_item/img/slide_2.jpg">
-                    </div>
-                    <div class="swiper-slide">
-                        <img src="./uni_item/img/slide_3.jpg">
-                    </div>
-                    <div class="swiper-slide">
-                        <img src="./uni_item/img/slide_4.jpg">
-                    </div>
+                    <? foreach ($uni['galerey'] as $item) {
+                        echo ('<div class="swiper-slide"><img src="' . $item . '"></div>');
+                    } ?>
                 </div>
-
-
-
             </div>
             <!-- If we need pagination -->
             <div class="swiper-pagination"></div>
@@ -271,10 +228,12 @@ wp_reset_postdata(); // Сбрасываем $post
 
     <?php
 
-    echo('<pre>');
-    print_r($uni);
-    echo('</pre>');
+    get_template_part('contact_us');
 
+    get_template_part('contacts');
+
+    // Модальное окно
+    get_template_part('connect-window');
 
     get_footer();
 
